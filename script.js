6e9d6205164f5b92acd767791f1b0702cb98c262
Harshini Kambhampalli <300549945+hkam7@users.noreply.github.com>
2026-07-29 23:50:19 +0000

    modified script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Welcome to my website!")
+var navBar = document.getElementById("navbar");
+navBar.style.backgroundColor = "lightbrown";
+navBar.style.color = "darkbrown";
+navBar.style.fontFamily = "dancing script, cursive";
